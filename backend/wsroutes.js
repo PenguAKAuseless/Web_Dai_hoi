e5feb0b3c2f098216d8b12fe2f12e7d48500e9bb
Havria <137485324+PenguAKAuseless@users.noreply.github.com>
2025-03-20 15:24:03 +0700

    ATTEMP #5

--- backend/wsroutes.js
+++ backend/wsroutes.js
@@ -29,7 +29,7 @@ const setupWebSocket = (server) => {
 
                     case 'CHECKIN_ATTENDANCE':
                         const { registrationId } = payload;
-                        console.log(typeof registrationId);
+                        console.log("ASCII Codes:", registrationId.split("").map(c => c.charCodeAt(0)).join(" "));
                         const logResult = await pool.query('SELECT * FROM attendance_log where delegate_id = $1', [registrationId]);
                         if (logResult.rows.length > 0) {
                             ws.send(JSON.stringify({ type: 'ERROR', payload: { message: 'Already checked in' } }));
